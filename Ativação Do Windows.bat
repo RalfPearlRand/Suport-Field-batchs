85429bbd270ca6b873369bf2fb28f63617d9f166
OrochiSage <66320173+RalfPearlRand@users.noreply.github.com>
2024-08-19 21:02:11 -0300

    Add files via upload

---
+++ Ativação Do Windows.bat
@@ -0,0 +1,4 @@
+cscript "C:\Windows\System32\slmgr.vbs" -skms 10.28.200.64
+cscript "C:\Windows\System32\slmgr.vbs" /ipk xxxxx-xxxxx-xxxxx-xxxxx-xxxxx
+cscript "C:\Windows\System32\slmgr.vbs" -ato
+pause
