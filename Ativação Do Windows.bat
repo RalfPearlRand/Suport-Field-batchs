55f2ee24af94ee566750cb46824d3f19365d9351
OrochiSage <66320173+RalfPearlRand@users.noreply.github.com>
2026-06-10 21:23:57 -0300

    Update Ativação Do Windows.bat

--- Ativação Do Windows.bat
+++ Ativação Do Windows.bat
@@ -1,4 +1,4 @@
-cscript "C:\Windows\System32\slmgr.vbs" -skms 10.28.200.64
+cscript "C:\Windows\System32\slmgr.vbs" -skms xxx.xxx.xxx.xxx
 cscript "C:\Windows\System32\slmgr.vbs" /ipk xxxxx-xxxxx-xxxxx-xxxxx-xxxxx
 cscript "C:\Windows\System32\slmgr.vbs" -ato
 pause
